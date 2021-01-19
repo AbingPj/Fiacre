62282edb892bf1c977c0316c8f5dde7c32e2e834
Ideahub Tech Development <ideahubdev@ideahubsolutionsinc.com>
2021-01-19 22:12:22 +0800

    Change text and logo for Fiacre

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,13 +1,13 @@
 @extends('frontend.layouts.landing')
 
 @section('title', app_name())
-@section('meta_description', 'Ultra-fresh, Locally Grown Produce. SunFarmacy is committed to making farming sustainable for both farm workers and consumers. So, we invite you to be a part of the food cycle that cares for people and the environment as SunFarmacy opens its membership for people who follow such high values. From farm to your table, let’s keep it local and grow better together!')
+@section('meta_description', 'Fiacre Foods is a new and innovative way for Catholic schools and parishes to raise substantial amounts of revenue by capitalizing on the everyday purchasing power of their families. Families simply sign up for grocery items through the Fiacre website that they already purchase on a regular basis at their local big box stores. Fiacre then shares the revenue with the schools and parishes.')
 @section('meta')
-    <meta property="og:title" content="Sunfarmacy" />
+    <meta property="og:title" content="Fiacre Foods" />
     <meta property="og:url"content="{{ url()->current() }}" />
-    <meta property="og:image" content="{{url('img/sunfarmacy/sun-og-image-1.jpg') }}" />
-    <meta property="og:image:alt" content="Sunfarmacy" />
-    <meta property="og:description" content="Ultra-fresh, Locally Grown Produce. SunFarmacy is committed to making farming sustainable for both farm workers and consumers. So, we invite you to be a part of the food cycle that cares for people and the environment as SunFarmacy opens its membership for people who follow such high values. From farm to your table, let’s keep it local and grow better together!" />
+    <meta property="og:image" content="{{url('img/fiacre/fiacre.jpg') }}" />
+    <meta property="og:image:alt" content="Fiacre Foods" />
+    <meta property="og:description" content="Fiacre Foods is a new and innovative way for Catholic schools and parishes to raise substantial amounts of revenue by capitalizing on the everyday purchasing power of their families. Families simply sign up for grocery items through the Fiacre website that they already purchase on a regular basis at their local big box stores. Fiacre then shares the revenue with the schools and parishes." />
 @endsection
 
 @section('content')
