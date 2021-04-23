f524c1e155bebc6f82ec7ba6ce4451378504c06a
AbingLeopoldo <leopoldo.a@filipinowebmasters.com>
2021-04-23 16:45:19 +0800

    Optimize Controller code of Fiacre Place Order

---
+++ resources/views/frontend/mail/order_receipt3.blade.php
@@ -0,0 +1,180 @@
+@extends('beautymail::templates.sunny')
+
+@section('content')
+
+    @include('beautymail::templates.sunny.contentStart')
+    <style>
+        .table{
+            width: 100%;
+            border: 1px solid #ddd;
+            /* max-width: 100%;
+            margin-bottom: 20px; */
+            /* background-color: transparent;
+            border-collapse: collapse;
+            border-spacing: 0; */
+        }
+
+        .cart-item-image {
+            width: 50px;
+            height: 50px;
+            padding: 0px;
+            margin-right: 50px !important;
+            background-color: white;
+          }
+          .store-logo{
+            width: auto;
+            height: 150px;
+            display: inline-block;
+          }
+    </style>
+
+<div style="text-align:center;">
+    <img class="store-logo" src="{{$store_logo}}" alt="">
+</div>
+    <br>
+    <h1 style="text-align:center"><b>Order Confirmation</b></h1>
+    <br>
+     <p>Hello <b>{{$customer_name}}</b></p>
+    <br>
+    <p>
+    Thank you for your order with
+    <b>{{$online_store_name}}</b>
+     We have received your
+    order dated
+    <b>{{$order_date}}</b>
+     and will process it shortly.
+    </p>
+    <br>
+    <h2>
+        ORDER #{{$order_number}}
+    </h2>
+    <br>
+    <h2>Your Order</h2>
+    <table class="table">
+        <thead>
+            <tr>
+                <th style="text-align:left" width="60px" >Item</th>
+                <th style="text-align:left"></th>
+                <th style="text-align:left">subscription price</th>
+                <th style="text-align:left"></th>
+                <th style="text-align:left">Total</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($products as $item)
+                <tr>
+                <td>
+                 <img class="cart-item-image" src="{{$item->product->image_link}}">
+                </td>
+                <td>
+                    {{$item->product->name}}
+                </td>
+                <td>
+                    @if($item->is_subscription)
+                        {{$item->price}} * {{$item->subscription_weeks}} weeks
+                    @else
+                        {{$item->atr_price_f}} * {{$item->updated_quantity}}/{{$item->atr_product_details->unit}}
+                    @endif
+                </td>
+                <td></td>
+                <td>
+                    @if($item->is_subscription)
+                        {{$item->subscription_price}}
+                    @else
+                        {{$item->atr_sub_total_f}}
+                    @endif
+                </td>
+            </tr>
+            @endforeach
+
+            {{-- <td>
+                {{$item->updated_quantity}}
+            </td>
+            <td>{{$item->price}}</td>
+            <td>{{$item->price * $item->updated_quantity}}</td> --}}
+
+
+            {{-- <tr>
+                <td>
+                    <img class="cart-item-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRVh6t-D6yOEQ2lpI_uNJMIm_RXSPlQXf1HCl4CsIkcIRlQHb7_&usqp=CAU">
+                </td>
+                <td>
+                    Product 1
+                    <br>
+                    #123sad123
+                </td>
+                <td>
+                    10
+                </td>
+                <td>80</td>
+                <td>800</td>
+            </tr> --}}
+            <tr style="border-collapse:collapse; height:30px;">
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>Order Amount</td>
+                <td>{{$total_amount}}</td>
+            </tr>
+            <tr style="border-collapse:collapse; height:30px;">
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>
+                    {{$billing_type}}
+                </td>
+                <td>{{$billing_method_price}}</th>
+            </tr>
+            <tr style="border-collapse:collapse; height:30px;">
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>
+                   Discount
+                </td>
+                <td>{{$order->referral_amount}}</th>
+            </tr>
+            <tr style="border-collapse:collapse; height:30px;">
+                <td></td>
+                <td></td>
+                <td></td>
+                <td><b>Total Amount</b></td>
+                <td><b>$ {{$overalltotal}}</b></td>
+            </tr>
+        </tbody>
+    </table>
+    <br>
+    <p>
+    {{-- Expected Delivery Date: <b>{{$order->atr_expected_delivery_date}}</b> --}}
+    </p>
+    <br>
+    <p>
+    PAYMENT METHOD
+    <ul>
+        <li>Online Payment</li>
+    </ul>
+    </p>
+    <br>
+    <h2>
+        <b>Thanks for shopping with us!</b>
+    </h2>
+    <p>You can check the status of your orders at any time on our Orders Page .</p>
+    <br>
+    <p>We welcome you to our store anytime. If you need assistance or have any questions,
+    please email us at
+     {{$online_store_email}}
+      We are happy to help!
+    </p>
+    <br>
+    <p>Sincerely,<br>
+        <b>{{$online_store_name}}</b>
+    </p>
+    <br>
+    <br>
+    © {{$online_store_name}} <br>
+     {{$online_store_address}}
+
+    @include('beautymail::templates.sunny.contentEnd')
+
+
+@stop
