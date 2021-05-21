b074a5b663f8908878789a8182c8d4221faa0097
Ideahub Tech Development <ideahubdev@ideahubsolutionsinc.com>
2021-05-21 17:42:05 +0800

    change meta

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -3,11 +3,11 @@
 @section('title', app_name())
 @section('meta_description', 'Fiacre Foods is a new and innovative way for Catholic schools and parishes to raise substantial amounts of revenue by capitalizing on the everyday purchasing power of their families. Families simply sign up for grocery items through the Fiacre website that they already purchase on a regular basis at their local big box stores. Fiacre then shares the revenue with the schools and parishes.')
 @section('meta')
-    <meta property="og:title" content="Fiacre Foods" />
+    <meta property="og:title" content="OHeavenly" />
     <meta property="og:url"content="{{ url()->current() }}" />
-    <meta property="og:image" content="{{url('img/fiacre/fiacre.jpg') }}" />
-    <meta property="og:image:alt" content="Fiacre Foods" />
-    <meta property="og:description" content="Fiacre Foods is a new and innovative way for Catholic schools and parishes to raise substantial amounts of revenue by capitalizing on the everyday purchasing power of their families. Families simply sign up for grocery items through the Fiacre website that they already purchase on a regular basis at their local big box stores. Fiacre then shares the revenue with the schools and parishes." />
+    <meta property="og:image" content="{{url('img/fiacre/fiacre-icon.png') }}" />
+    <meta property="og:image:alt" content="OHeavenly" />
+    <meta property="og:description" content="OHeavenly is a new and innovative way for Catholic schools and parishes to raise substantial amounts of revenue by capitalizing on the everyday purchasing power of their families. Families simply sign up for grocery items through the Fiacre website that they already purchase on a regular basis at their local big box stores. Fiacre then shares the revenue with the schools and parishes." />
 @endsection
 
 @section('content')
